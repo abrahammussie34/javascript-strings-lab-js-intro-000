c330911314f99b11be1d37d16c78dbdeb1de9e70
Jabe Rooney <abrahamsahlzghi@gmail.com>
2019-02-10 02:00:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var greeting = "Hello, everybody";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "welcome";
+var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 
